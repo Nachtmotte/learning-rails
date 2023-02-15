74b02d706f3e943285c435c9cbf52c2459285cc1
Nachtmotte <matias_palomeque@hotmail.com>
2023-02-15 00:29:18 -0300

    added filters

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ApplicationRecord
+    include PgSearch::Model
+    pg_search_scope :search_full_text, against: {
+        tittle: 'A',
+        description: 'B'
+    }
+
+    ORDER_BY = {
+        newest: "created_at DESC",
+        expensive: "price DESC",
+        cheapest: "price ASC"
+    }
+
     has_one_attached :photo
     
     validates :tittle, presence: true
