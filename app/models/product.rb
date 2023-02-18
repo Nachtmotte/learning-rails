60107deac29149a950c5f3c91bdff1a8432bce0c
Nachtmotte <matias_palomeque@hotmail.com>
2023-02-17 21:16:16 -0300

    added favorites

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,26 +1,28 @@
 class Product < ApplicationRecord
     include PgSearch::Model
+    include Favoritable
+  
     pg_search_scope :search_full_text, against: {
-        tittle: 'A',
-        description: 'B'
+      tittle: 'A',
+      description: 'B'
     }
-
+  
     ORDER_BY = {
-        newest: "created_at DESC",
-        expensive: "price DESC",
-        cheapest: "price ASC"
+      newest: "created_at DESC",
+      expensive: "price DESC",
+      cheapest: "price ASC"
     }
-
+  
     has_one_attached :photo
-    
+  
     validates :tittle, presence: true
     validates :description, presence: true
     validates :price, presence: true
-
+  
     belongs_to :category
     belongs_to :user, default: -> { Current.user }
-
-    def owner? 
-        user_id == Current.user&.id
+  
+    def owner?
+      user_id == Current.user&.id
     end
-end
+  end
